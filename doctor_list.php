e1f93db4677edfc381a82072f31f6e8fe69ec122
nam <162106480+csbluvrr@users.noreply.github.com>
2025-01-10 11:20:23 +0100

    Update doctor_list.php

--- doctor_list.php
+++ doctor_list.php
@@ -42,7 +42,7 @@ if (isset($_GET['search'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="icon" type="image/x-icon" href="images\\emsiicon.png">
+   <link rel="icon" type="image/x-icon" href="images\hopitallogo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor List</title>
